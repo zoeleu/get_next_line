b55c5b307f99907551a052bc18d66211999e0af6
Zoe <zleullie@student.42sp.org.br>
2026-05-23 17:33:59 -0300

    slightly working?

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zleullie <zleullie@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/23 14:43:07 by zleullie          #+#    #+#             */
+/*   Updated: 2026/05/23 17:33:18 by zleullie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+
+char	*get_next_line(int fd);
+
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char *s);
+char	*ft_strncat(char *dest, char *buf, size_t n);
+char	*ft_strndup(char *s1, size_t n);
+char	*find_end(char *s1, int str_end);
+char	*cleanup(char **stash);
+
+#endif
